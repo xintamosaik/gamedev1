3548484d2e86fd220dc6618e7b2855eecebe62ce
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-02-15 13:10:03 +0100

    tidy

--- src/render.ts
+++ src/render.ts
@@ -2,13 +2,15 @@ import { Position, Render, Dimensions } from './types';
 
 export const canvas = document.getElementById('game') as HTMLCanvasElement;
 export const context = canvas.getContext('2d')!;
-const renderables: Renderable[] = [];
 
 export interface Renderable {
     position: Position;
     dimensions: Dimensions,
     render: Render;
 }
+
+const renderables: Renderable[] = [];
+
 function isRenderable(thing: unknown): thing is Renderable {
     return (
         typeof thing === 'object' &&
@@ -25,8 +27,8 @@ export function registerRenderable(thing: unknown) {
 }
 
 export function renderAll() {
-    for (const e of renderables) {
-        context.fillStyle = e.render.color;
-        context.fillRect(e.position.x, e.position.y, e.dimensions.w, e.dimensions.h);
-    }
+  for (const e of renderables) {
+    context.fillStyle = e.render.color;
+    context.fillRect(e.position.x, e.position.y, e.dimensions.w, e.dimensions.h);
+  }
 }
