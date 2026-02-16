194eec5ae6d06242c440a8ba8d5977c4fd0d5e9d
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-02-16 16:16:42 +0100

    autoformat

--- src/game.ts
+++ src/game.ts
@@ -4,11 +4,11 @@ import { updateMovement } from './movement';
 import { canvas, context, renderAll } from './render';
 import levelOne from './levels/one';
 import { Background } from 'levels/types';
-import { 
-    isSolid, 
-    registerSolid, 
-    checkCollisions, 
-    isInteractable, 
+import {
+    isSolid,
+    registerSolid,
+    checkCollisions,
+    isInteractable,
     registerInteractable,
     checkProximity
 } from 'collision';
@@ -21,25 +21,25 @@ let previousFrameTime = 0;
 const MAX_DELTA_TIME = 0.05;
 const MOVEMENT_SCALE = 200; // max 50ms step
 
-const player =   {
-        id: ID(),
-        position: { x: 100, y: 100 },
-        dimensions: { w: 50, h: 50 },
-        velocity: { vx: 0, vy: 0 },
-        render: { color: '#d5a442' },
-        aura: 20,
+const player = {
+    id: ID(),
+    position: { x: 100, y: 100 },
+    dimensions: { w: 50, h: 50 },
+    velocity: { vx: 0, vy: 0 },
+    render: { color: '#d5a442' },
+    aura: 20,
 }
 registerMovable(player);
 registerRenderable(player);
 
 function isRecord(x: unknown): x is Record<string, unknown> {
-  return typeof x === 'object' && x !== null;
+    return typeof x === 'object' && x !== null;
 }
 function createLevel(descriptions: unknown[]) {
     for (const description of descriptions) {
         if (!isRecord(description)) continue;
-        const thing = { ...description, id: ID()} 
-  
+        const thing = { ...description, id: ID() }
+
         if (isRenderable(thing)) {
             registerRenderable(thing);
         }
@@ -50,31 +50,29 @@ function createLevel(descriptions: unknown[]) {
         if (isInteractable(thing)) {
             registerInteractable(thing);
         }
-   
+
     }
 }
 createLevel(levelOne.statics);
 let activeLevel = levelOne;
 
 function drawBackground(bg: Background) {
-    if (bg.color ) {
+    if (bg.color) {
         context.fillStyle = bg.color;
         context.fillRect(0, 0, canvas.width, canvas.height);
     }
 }
 
-
-
 function gameLoop(timestamp: number): void {
 
     const deltaTime = Math.min(
-        (timestamp - previousFrameTime) / 1000, 
+        (timestamp - previousFrameTime) / 1000,
         MAX_DELTA_TIME
-    ); 
+    );
 
     updateInputLogic(player.velocity);
     updateMovement(deltaTime, MOVEMENT_SCALE);
-  
+
     const collisions = checkCollisions(player.position, player.dimensions);
     if (DEBUG) player.render.color = collisions.length > 0 ? '#f00' : '#d5a442';
     const near = checkProximity(player.position, player.dimensions, player.aura);
@@ -84,7 +82,7 @@ function gameLoop(timestamp: number): void {
 
     if (DEBUG) context.fillStyle = '#0f0';
     if (DEBUG) context.fillText(`FPS: ${Math.round(1 / deltaTime)}`, 10, 30);
-    
+
     previousFrameTime = timestamp;
     window.requestAnimationFrame(gameLoop);
 }
